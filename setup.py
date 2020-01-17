a2577ea26bebbf9f28f8c36b49a820609e13e7c6
EtienneDx <etiennedx00@gmail.com>
2020-01-17 11:12:12 +0100

    added submodules and setup script

---
+++ setup.py
@@ -0,0 +1,21 @@
+import subprocess
+import sys
+import importlib
+
+if sys.version_info[0] < 3:
+    raise Exception("Must be using Python 3.x!")
+
+subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "FinalRecon/requirements.txt"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "LittleBrtother/requirements.txt"])
+
+found = False
+if sys.version_info[1] < 4:
+    spam_loader = importlib.find_loader('tkinter')
+    found = spam_loader is not None
+else:
+    spam_spec = importlib.util.find_spec("spam")
+    found = spam_spec is not None
+
+if not found:
+    print("You need to install tkinter for the GUI to work properly!")
+    print("See https://tkdocs.com/tutorial/install.html")

cc9578f2c77b349873087a2709b283699d50a023
EtienneDx <etiennedx00@gmail.com>
2020-01-17 14:56:06 +0100

    changed test of tkinter in setup

--- setup.py
+++ setup.py
@@ -8,14 +8,8 @@ if sys.version_info[0] < 3:
 subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "FinalRecon/requirements.txt"])
 subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "LittleBrother/requirements.txt"])
 
-found = False
-if sys.version_info[1] < 4:
-    spam_loader = importlib.find_loader('tkinter')
-    found = spam_loader is not None
-else:
-    spam_spec = importlib.util.find_spec("spam")
-    found = spam_spec is not None
+import tkinter
 
-if not found:
+if tkinter is None:
     print("You need to install tkinter for the GUI to work properly!")
     print("See https://tkdocs.com/tutorial/install.html")
